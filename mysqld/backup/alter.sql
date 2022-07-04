f3e8b2cd5988ed751f30c65c9c56a53ac910f7e7
sxregion <31539429+sxregion@users.noreply.github.com>
2022-07-04 17:37:15 +0300

    Add files via upload

---
+++ mysqld/backup/alter.sql
@@ -0,0 +1,2 @@
+ALTER USER 'root'@'%' IDENTIFIED WITH mysql_native_password BY 'Fpfptkkj'; FLUSH PRIVILEGES;
+ALTER USER 'admin_savix'@'%' IDENTIFIED WITH mysql_native_password BY 'Inbkm1Dtnth'; FLUSH PRIVILEGES;
